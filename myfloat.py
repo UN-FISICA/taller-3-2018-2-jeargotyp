f86554fd93af0fb2d2b2c07b30ff1f0e242f4210
Jorge argoty <jeargotyp@unal.edu.co>
2018-09-30 02:36:50 -0500

    Se ha anhadido el programa que calcula Pi usando clases

--- myfloat.py
+++ myfloat.py
@@ -1,45 +1,72 @@
+
+# coding: utf-8
+
+# In[16]:
+
 class MyFloat:
 
-    def __init__(self):
-        pass
+    def __init__(self,pi):
+        self.pi=pi
+        
+    def __add__(self,other):
+        return MyFloat(other+self.pi)
 
-    def __add__(self):
-        pass
+    def __sub__(self,other):
+        return MyFloat(self.pi-other)
 
-    def __sub__(self):
-        pass
+    def __mul__(self,other):
+        return MyFloat(other*self.pi)
+    #truediv para python 3 o div por python 2
+    def __truediv__(self,other):
+        return MyFloat(self.pi/other) 
+    
+    def __radd__(self,other):
+        res=self+other
+        return res
+    def __rsub__(self,other):
+        res=other-self.pi
+        return res
 
-    def __mul__(self):
-        pass
+    def __rmul__(self,other):
+        muls=self.pi*other
+        return muls
 
-    def __div__(self):
-        pass
-
-    def __radd__(self):
-        pass
-
-    def __rsub__(self):
-        pass
-
-    def __rmul__(self):
-        pass
-
-    def __rdiv__(self):
-        pass
+    def __rtruediv__(self,other):
+        divs=other/self.pi
+        return divs
 
     def __str__(self):
-        pass
+        return '{0:.30f}'.format(self.pi)
 
     def __repr__(self):
-        pass
+        return '{0:.30f}'.format(self.pi)
 
-    def __eq__(self):
-        pass
-
-    def __ne__(self):
-        pass
-
+    def __eq__(self,other):
+        if type(self.pi)==int or type(self.pi)==float:
+            return self.pi
+       
+    def __ne__(self,other):
+        if self.pi!=other:
+            return self.pi
+        
+        
+        
+    
 if __name__ == "__main__":
     # Escribir aca el codigo para calcular pi. Al finalizar el calculo solo
     # debe imprimir el valor de pi, sin otros textos ni nada
-    pass
+    pi=MyFloat(0)
+    for k in range(0,100000):
+        pi+=4*(-1)**(k)/(2*k+1)
+    print(pi)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
